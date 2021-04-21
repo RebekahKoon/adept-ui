2f858252e215688189fcc6b1767da17c01c9de7b
Nathan <Nathan.shelby30@gmail.com>
2021-04-21 12:47:02 -0700

    Learning with Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+// componenets/Footer.js
+import React from "react";
+
+const FooterStyle = {
+    backgroundColor: "blue",
+    color: "white",
+    width: "100%",
+    height: "50px"
+};
+
+const Footer = () => (
+    <div className="Footer" style={FooterStyle}>
+        FOOTER
+    </div>
+);
+
+export default Footer;
