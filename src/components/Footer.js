e8e1f814281fecd46405ebb57d77634bc66ef6b7
Nathan <Nathan.shelby30@gmail.com>
2021-04-23 13:58:45 -0700

    Using Styled Correctly now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,107 +9,47 @@ import Image from 'next/image';
 
 import styled from 'styled-components';
 
-const StyledFooter = styled.footer`
-    .Footer {
+import StyledFooter from './FooterStyle.js';
 
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: fixed;
-        bottom: 0;
+import {StyledLogo, StyledRow1, StyledRow2, StyledRow3, StyledLinks, StyledUl, StyledLi, StyledLiHeader} from './FooterStyle.js';
 
-        height: 200px;
-        width: 100%;
-
-        padding: 0 20px;
-
-        font-family: 'PT Sans', sans-serif;
-        font-size: 24px;
-        color: #FFFFFF;
-        text-transform: uppercase;
-
-        background: #191C3C;
-
-        cursor: pointer;
-
-    }
-    #Logo {
-        padding: 10%;
-        flex: 1 10%;
-    }
-    #Row1, #Row2, #Row3 {
-        font-size: 14px;
-    }
-
-    #Row1 {
-        flex: 2 20%;
-    }
-
-    #Row2 {
-        flex: 3 20%;
-    }
-
-    #Row3 {
-        flex: 4 20% ;
-    }
-
-    #Links {
-        flex: 5 20%;
-    }
-
-    .UL1, .UL2, .UL3 {
-        list-style-type: none;
-        margin-bottom: 10px;
-    }
-
-    #LI {
-        margin: 0 10px 10px 0;
-        text-transform: none;
-    }
-
-    #LIHeader {
-        margin: 0 10px 10px 0;
-        color: #919496;
-    }
-`;
+  
 
 const Footer = props => (
     <StyledFooter>
-        <div className="Footer">
-            <div id="Logo">
+            <StyledLogo>
                 <Image src='/TextLogo.png' alt='TextLogo' width='150' height='70' />
-            </div>
-            <div className="FooterData" id="Row1">
-                <ul className="UL1">
-                    <li id="LIHeader">FOR JOB SEEKERS</li>
-                    <li id= "LI">Browse Jobs</li>
-                    <li id= "LI">Browse Contacts</li>
-                    <li id= "LI">Skills</li>
-                    <li id= "LI">Visualize Data</li>
-                </ul>
-            </div>
-            <div className="FooterData" id="Row2">
-                <ul className="UL2">
-                    <li id="LIHeader">FOR EMPLOYERS</li>
-                    <li id= "LI">Post a Job</li>
-                    <li id= "LI">View Posted Jobs</li>
-                    <li id= "LI">View Applicants</li>
-                </ul>
-            </div>
-            <div className="FooterData" id="Row3">
-                <ul className="UL3">
-                    <li id="LIHeader">RESOURCES</li>
-                    <li id= "LI">About Adept</li>
-                    <li id= "LI">Contact</li>
-                    <li id= "LI">View Repository</li>
-                </ul>
-            </div>
-            <div className="FooterData" id="Links">
+            </StyledLogo>
+            <StyledRow1>
+                <StyledUl>
+                    <StyledLiHeader>FOR JOB SEEKERS</StyledLiHeader>
+                    <StyledLi>Browse Jobs</StyledLi>
+                    <StyledLi>Browse Contacts</StyledLi>
+                    <StyledLi>Skills</StyledLi>
+                    <StyledLi>Visualize Data</StyledLi>
+                </StyledUl>
+            </StyledRow1>
+            <StyledRow2>
+                <StyledUl>
+                    <StyledLiHeader>FOR EMPLOYERS</StyledLiHeader>
+                    <StyledLi>Post a Job</StyledLi>
+                    <StyledLi>View Posted Jobs</StyledLi>
+                    <StyledLi>View Applicants</StyledLi>
+                </StyledUl>
+            </StyledRow2>
+            <StyledRow3>
+                <StyledUl>
+                    <StyledLiHeader>RESOURCES</StyledLiHeader>
+                    <StyledLi>About Adept</StyledLi>
+                    <StyledLi>Contact</StyledLi>
+                    <StyledLi>View Repository</StyledLi>
+                </StyledUl>
+            </StyledRow3>
+            <StyledLinks>
                 <Image src='/GithubIcon1.png' alt='me' width='40' height='40'/>
                 <Image src='/GithubIcon2.png' alt='me' width='40' height='40'/>
                 <Image src='/GithubIcon3.png' alt='me' width='40' height='40'/>
-            </div>
-        </div>
+            </StyledLinks>
     </StyledFooter>
 
 );
