febf3f9ae7b77f2a971e9c6472b3e957bd1ae229
Nathan <Nathan.shelby30@gmail.com>
2021-05-01 16:06:04 -0700

    Push for Mid-Point Project

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -30,7 +30,7 @@ const Layout = (props) => {
       <StyledContentContainer>
         <GlobalStyle />
 
-        <SearchBar />
+        <NavBar />
 
         <StyledContent>{props.children}</StyledContent>
 
