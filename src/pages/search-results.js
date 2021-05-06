1ea181564052097b58de008f76de8fe67ffc1887
Nathan <Nathan.shelby30@gmail.com>
2021-05-05 19:23:17 -0700

    Better checkbox implementation

--- src/pages/search-results.js
+++ src/pages/search-results.js
@@ -1,5 +1,6 @@
 // pages/dashboard.js
 import React from 'react'
+import { useState } from 'react'
 import Layout from '../components/Layout'
 import SearchBar from '../components/SearchBar'
 import StyledSearchResults from '../styles/SearchResultsStyle'
@@ -35,73 +36,71 @@ const Checkbox = ({ className, checked, ...props }) => (
     </StyledCheckbox>
   </CheckboxContainer>
 )
-class SearchResultView extends React.Component {
-  state = { checked: false }
+function SearchResultView(props) {
+  const [checked, setChecked] = useState(false)
 
-  handleCheckboxChange = (event) =>
-    this.setState({ checked: event.target.checked })
-
-  render() {
-    return (
-      <Layout>
-        {
-          <SearchResultsParent>
-            <SearchBar />
-            <StyledSearchResults>
-              <SSRMain>
-                <SSRSearchResultsHeader>
-                  69,420 results found
-                  <SSRSortByDropdown>sort by: newest</SSRSortByDropdown>
-                </SSRSearchResultsHeader>
-                <SSRMainContentContainer>
-                  <SSRFilterSideBar>
-                    <SSRFilterSection>
-                      JobType
-                      <SSRFilterOptions>
-                        <SSRCheckBoxOption>
-                          <label>
-                            <Checkbox
-                              checked={this.state.checked}
-                              onChange={this.handleCheckboxChange}
-                            />
-                            <span>Label Text</span>
-                          </label>
-                        </SSRCheckBoxOption>
-                      </SSRFilterOptions>
-                    </SSRFilterSection>
-                    <SSRFilterSection>
-                      Job Category
-                      <SSRFilterOptions>
-                        <SSRCheckBoxOption></SSRCheckBoxOption>
-                      </SSRFilterOptions>
-                    </SSRFilterSection>
-                    <SSRFilterSection>
-                      Experience
-                      <SSRFilterOptions>
-                        <SSRCheckBoxOption></SSRCheckBoxOption>
-                      </SSRFilterOptions>
-                    </SSRFilterSection>
-                  </SSRFilterSideBar>
-                  <SSRSearchResults>Bye</SSRSearchResults>
-                </SSRMainContentContainer>
-                <SSRMainContentFooter>
-                  <SSRPagination>
-                    <SSRPagePrev>{'<'}</SSRPagePrev>
-                    <SSRPageNumber>{'1'}</SSRPageNumber>
-                    <SSRPageNumber>{'2'}</SSRPageNumber>
-                    <SSRPageNumber>{'3'}</SSRPageNumber>
-                    <SSRPageNumber>{'...'}</SSRPageNumber>
-                    <SSRPageNumber>{'11'}</SSRPageNumber>
-                    <SSRPageNext>{'>'}</SSRPageNext>
-                  </SSRPagination>
-                </SSRMainContentFooter>
-              </SSRMain>
-            </StyledSearchResults>
-          </SearchResultsParent>
-        }
-      </Layout>
-    )
+  const handleCheckboxChange = (event) => {
+    setChecked(event.target.checked)
   }
+  return (
+    <Layout>
+      {
+        <SearchResultsParent>
+          <SearchBar />
+          <StyledSearchResults>
+            <SSRMain>
+              <SSRSearchResultsHeader>
+                69,420 results found
+                <SSRSortByDropdown>sort by: newest</SSRSortByDropdown>
+              </SSRSearchResultsHeader>
+              <SSRMainContentContainer>
+                <SSRFilterSideBar>
+                  <SSRFilterSection>
+                    JobType
+                    <SSRFilterOptions>
+                      <SSRCheckBoxOption>
+                        <label>
+                          <Checkbox
+                            checked={checked}
+                            onChange={handleCheckboxChange}
+                          />
+                          <span>Label Text</span>
+                        </label>
+                      </SSRCheckBoxOption>
+                    </SSRFilterOptions>
+                  </SSRFilterSection>
+                  <SSRFilterSection>
+                    Job Category
+                    <SSRFilterOptions>
+                      <SSRCheckBoxOption></SSRCheckBoxOption>
+                    </SSRFilterOptions>
+                  </SSRFilterSection>
+                  <SSRFilterSection>
+                    Experience
+                    <SSRFilterOptions>
+                      <SSRCheckBoxOption></SSRCheckBoxOption>
+                    </SSRFilterOptions>
+                  </SSRFilterSection>
+                </SSRFilterSideBar>
+                <SSRSearchResults>Bye</SSRSearchResults>
+              </SSRMainContentContainer>
+              <SSRMainContentFooter>
+                <SSRPagination>
+                  <SSRPagePrev>{'<'}</SSRPagePrev>
+                  <SSRPageNumber>{'1'}</SSRPageNumber>
+                  <SSRPageNumber>{'2'}</SSRPageNumber>
+                  <SSRPageNumber>{'3'}</SSRPageNumber>
+                  <SSRPageNumber>{'...'}</SSRPageNumber>
+                  <SSRPageNumber>{'11'}</SSRPageNumber>
+                  <SSRPageNext>{'>'}</SSRPageNext>
+                </SSRPagination>
+              </SSRMainContentFooter>
+            </SSRMain>
+          </StyledSearchResults>
+        </SearchResultsParent>
+      }
+    </Layout>
+  )
 }
 
 export default SearchResultView
