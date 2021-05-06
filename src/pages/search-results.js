4931ce418bf50403003203f7c808a94e0cb4a3c8
Nathan <Nathan.shelby30@gmail.com>
2021-05-05 19:08:12 -0700

    Playing with checkbox

--- src/pages/search-results.js
+++ src/pages/search-results.js
@@ -9,24 +9,99 @@ import {
   SSRSearchResults,
   SSRMain,
   SSRMainContentContainer,
+  SSRSearchResultsHeader,
+  SSRSortByDropdown,
+  SSRMainContentFooter,
+  SSRPagination,
+  SSRPageNext,
+  SSRPagePrev,
+  SSRPageNumber,
+  SSRFilterSection,
+  SSRFilterOptions,
+  SSRCheckBoxOption,
+  SSRCheckBox,
+  CheckboxContainer,
+  StyledCheckbox,
+  Icon,
 } from '../styles/SearchResultsStyle'
 
-const SearchResultView = (props) => (
-  <Layout>
-    {
-      <SearchResultsParent>
-        <SearchBar />
-        <StyledSearchResults>
-          <SSRMain>
-            <SSRMainContentContainer>
-              <SSRFilterSideBar>Hi</SSRFilterSideBar>
-              <SSRSearchResults>Bye</SSRSearchResults>
-            </SSRMainContentContainer>
-          </SSRMain>
-        </StyledSearchResults>
-      </SearchResultsParent>
-    }
-  </Layout>
+const Checkbox = ({ className, checked, ...props }) => (
+  <CheckboxContainer className={className}>
+    <SSRCheckBox checked={checked} {...props} />
+    <StyledCheckbox checked={checked}>
+      <Icon viewBox="0 0 24 24">
+        <polyline points="20 6 9 17 4 12" />
+      </Icon>
+    </StyledCheckbox>
+  </CheckboxContainer>
 )
+class SearchResultView extends React.Component {
+  state = { checked: false }
+
+  handleCheckboxChange = (event) =>
+    this.setState({ checked: event.target.checked })
+
+  render() {
+    return (
+      <Layout>
+        {
+          <SearchResultsParent>
+            <SearchBar />
+            <StyledSearchResults>
+              <SSRMain>
+                <SSRSearchResultsHeader>
+                  69,420 results found
+                  <SSRSortByDropdown>sort by: newest</SSRSortByDropdown>
+                </SSRSearchResultsHeader>
+                <SSRMainContentContainer>
+                  <SSRFilterSideBar>
+                    <SSRFilterSection>
+                      JobType
+                      <SSRFilterOptions>
+                        <SSRCheckBoxOption>
+                          <label>
+                            <Checkbox
+                              checked={this.state.checked}
+                              onChange={this.handleCheckboxChange}
+                            />
+                            <span>Label Text</span>
+                          </label>
+                        </SSRCheckBoxOption>
+                      </SSRFilterOptions>
+                    </SSRFilterSection>
+                    <SSRFilterSection>
+                      Job Category
+                      <SSRFilterOptions>
+                        <SSRCheckBoxOption></SSRCheckBoxOption>
+                      </SSRFilterOptions>
+                    </SSRFilterSection>
+                    <SSRFilterSection>
+                      Experience
+                      <SSRFilterOptions>
+                        <SSRCheckBoxOption></SSRCheckBoxOption>
+                      </SSRFilterOptions>
+                    </SSRFilterSection>
+                  </SSRFilterSideBar>
+                  <SSRSearchResults>Bye</SSRSearchResults>
+                </SSRMainContentContainer>
+                <SSRMainContentFooter>
+                  <SSRPagination>
+                    <SSRPagePrev>{'<'}</SSRPagePrev>
+                    <SSRPageNumber>{'1'}</SSRPageNumber>
+                    <SSRPageNumber>{'2'}</SSRPageNumber>
+                    <SSRPageNumber>{'3'}</SSRPageNumber>
+                    <SSRPageNumber>{'...'}</SSRPageNumber>
+                    <SSRPageNumber>{'11'}</SSRPageNumber>
+                    <SSRPageNext>{'>'}</SSRPageNext>
+                  </SSRPagination>
+                </SSRMainContentFooter>
+              </SSRMain>
+            </StyledSearchResults>
+          </SearchResultsParent>
+        }
+      </Layout>
+    )
+  }
+}
 
 export default SearchResultView
--- src/styles/SearchResultsStyle.js
+++ src/styles/SearchResultsStyle.js
@@ -59,6 +59,84 @@ export const SSRFilterSideBar = styled.div`
   box-shadow: 0px 4px 10px rgba(80, 120, 239, 0.1);
   border-radius: 5px;
 `
+export const SSRFilterSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 200px;
+  height: 191px;
+`
+
+export const SSRFilterOptions = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 87px;
+  height: 160px;
+  left: 0px;
+  top: 31px;
+  background-color: blue;
+`
+
+export const SSRCheckBoxOption = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+
+  position: static;
+  width: 87px;
+  height: 40px;
+  left: 0px;
+  top: 120px;
+`
+export const SSRCheckBox = styled.input.attrs({ type: 'checkbox' })`
+  border: 0;
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`
+
+export const CheckboxContainer = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`
+
+export const Icon = styled.svg`
+  fill: none;
+  stroke: white;
+  stroke-width: 2px;
+`
+
+export const StyledCheckbox = styled.div`
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  background: ${(props) => (props.checked ? 'salmon' : 'papayawhip')};
+  border-radius: 3px;
+  transition: all 150ms;
+
+  ${SSRCheckBox}:focus + & {
+    box-shadow: 0 0 0 3px pink;
+  }
+
+  ${Icon} {
+    visibility: ${(props) => (props.checked ? 'visible' : 'hidden')};
+  }
+`
+
 export const SSRSearchResults = styled.div`
   display: flex;
   flex-direction: column;
@@ -76,6 +154,99 @@ export const SSRSearchResults = styled.div`
   box-shadow: 0px 4px 10px rgba(80, 120, 239, 0.1);
   border-radius: 5px;
 `
+export const SSRSearchResultsHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+
+  position: static;
+  width: 1108px;
+  height: 28px;
+`
+
+export const SSRSortByDropdown = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+
+  position: static;
+  width: 131px;
+  height: 22px;
+  max-width: var(--maxWidth);
+  margin-right: 11.25%;
+`
+
+export const SSRMainContentFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+
+  position: static;
+  width: 1108px;
+  height: 132px;
+  left: 16px;
+  top: 1057px;
+`
+
+export const SSRPagination = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 40px;
+
+  position: static;
+  width: 462px;
+  height: 132px;
+  left: 323px;
+  top: 0px;
+`
+
+export const SSRPagePrev = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 14px 19px;
+
+  position: static;
+  width: 62px;
+  height: 52px;
+  left: 40px;
+  top: 40px;
+`
+
+export const SSRPageNumber = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 40px;
+
+  position: static;
+  width: 462px;
+  height: 132px;
+  left: 323px;
+  top: 0px;
+`
+
+export const SSRPageNext = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 14px 19px;
+
+  position: static;
+  width: 62px;
+  height: 52px;
+  left: 360px;
+  top: 40px;
+`
+
 export const SearchResultsParent = styled.div``
 
 export default StyledSearchResults
