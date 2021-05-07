22282a97b7157c9c22117d20e821e53ef7d19d5a
Rebekah <koonr@oregonstate.edu>
2021-05-06 21:55:13 -0700

    filter for search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,80 +1,119 @@
 import React from 'react'
-
 import Router from 'next/router'
-
 import Link from 'next/link'
-
+import { useState } from 'react'
 import Image from 'next/image'
-
 import StyledSearchBar from './SearchBarStyle'
-
+import NavBar from '../NavBar'
 import {
+  StyledSearchHeader,
   StyledSearchContainer,
-  StyledSearchHeaderContainer,
-  StyledSearchBarContainer,
-  StyledSearchFormContainer,
-  StyledSearchButton,
-  StyledSearchFormInput,
-  StyledSearchIcon,
-  StyledSearchDivider,
-  StyledSearchDividerDiv,
-  StyledSearchDropdown,
-  StyledSearchDropdownArrow,
-  StyledSearchDropdownText,
+  StyledDropdown,
+  // StyledSearchContainer,
+  // StyledSearchHeaderContainer,
+  // StyledSearchBarContainer,
+  // StyledSearchFormContainer,
+  // StyledSearchButton,
+  // StyledSearchFormInput,
+  // StyledSearchIcon,
+  // StyledSearchDivider,
+  // StyledSearchDividerDiv,
+  // StyledSearchDropdown,
+  // StyledSearchDropdownArrow,
+  // StyledSearchDropdownText,
 } from './SearchBarStyle'
 
-import NavBar from '../NavBar'
+// export default class SearchBar extends React.Component {
+//   constructor(props) {
+//     super(props)
+//   }
 
-export default class SearchBar extends React.Component {
-  constructor(props) {
-    super(props)
+//   render() {
+//     const handleClick = (e) => {
+//       e.preventDefault()
+//       Router.push('/search-results')
+//     }
+//     return (
+//       <StyledSearchBar>
+//         <NavBar />
+//         <StyledSearchContainer>
+//           <StyledSearchHeaderContainer>
+//             Discover New Jobs and Connections
+//             <StyledSearchBarContainer>
+//               <StyledSearchDropdown>
+//                 <StyledSearchDropdownText>All</StyledSearchDropdownText>
+//                 <StyledSearchDropdownArrow>
+//                   <Image
+//                     src="/arrow_drop_down.png"
+//                     alt="Search Icon"
+//                     width="22"
+//                     height="22"
+//                   />
+//                 </StyledSearchDropdownArrow>
+//               </StyledSearchDropdown>
+//               <StyledSearchDivider>
+//                 <StyledSearchDividerDiv />
+//               </StyledSearchDivider>
+//               <StyledSearchFormContainer>
+//                 <div>
+//                   <form onSubmit={handleClick}>
+//                     <label htmlFor="JobSearch"></label>
+//                     <StyledSearchFormInput
+//                       id="JobSearch"
+//                       type="text"
+//                       placeholder="Search"
+//                     ></StyledSearchFormInput>
+//                     <StyledSearchIcon>
+//                       <StyledSearchButton type="submit"></StyledSearchButton>
+//                     </StyledSearchIcon>
+//                   </form>
+//                 </div>
+//               </StyledSearchFormContainer>
+//             </StyledSearchBarContainer>
+//           </StyledSearchHeaderContainer>
+//         </StyledSearchContainer>
+//       </StyledSearchBar>
+//     )
+//   }
+// }
+
+const options = [
+  {
+    label: 'Job Postings',
+    value: 'jobPostings',
+  },
+  {
+    label: 'Users',
+    value: 'users',
+  },
+]
+
+const SearchBar = () => {
+  const [option, setOption] = useState('All')
+  const handleOptionChange = (e) => {
+    setOption(e.target.value)
   }
 
-  render() {
-    const handleClick = (e) => {
-      e.preventDefault()
-      Router.push('/search-results')
-    }
-    return (
-      <StyledSearchBar>
-        <NavBar />
-        <StyledSearchContainer>
-          <StyledSearchHeaderContainer>
-            Discover New Jobs and Connections
-            <StyledSearchBarContainer>
-              <StyledSearchDropdown>
-                <StyledSearchDropdownText>All</StyledSearchDropdownText>
-                <StyledSearchDropdownArrow>
-                  <Image
-                    src="/arrow_drop_down.png"
-                    alt="Search Icon"
-                    width="22"
-                    height="22"
-                  />
-                </StyledSearchDropdownArrow>
-              </StyledSearchDropdown>
-              <StyledSearchDivider>
-                <StyledSearchDividerDiv />
-              </StyledSearchDivider>
-              <StyledSearchFormContainer>
-                <div>
-                  <form onSubmit={handleClick}>
-                    <label htmlFor="JobSearch"></label>
-                    <StyledSearchFormInput
-                      id="JobSearch"
-                      type="text"
-                      placeholder="Search"
-                    ></StyledSearchFormInput>
-                    <StyledSearchIcon>
-                      <StyledSearchButton type="submit"></StyledSearchButton>
-                    </StyledSearchIcon>
-                  </form>
-                </div>
-              </StyledSearchFormContainer>
-            </StyledSearchBarContainer>
-          </StyledSearchHeaderContainer>
-        </StyledSearchContainer>
-      </StyledSearchBar>
-    )
-  }
+  return (
+    <StyledSearchHeader>
+      <StyledSearchContainer>
+        <h1>Discover Jobs and Make Connections</h1>
+        <StyledDropdown>
+          <select
+            value={option}
+            onChange={handleOptionChange}
+            className="optionSelect"
+          >
+            <option value="all" label="ALL">
+              All
+            </option>
+            <option value="jobPostings" label="Job Postings"></option>
+            <option value="users" label="Users"></option>
+          </select>
+        </StyledDropdown>
+      </StyledSearchContainer>
+    </StyledSearchHeader>
+  )
 }
+
+export default SearchBar
--- src/components/SearchBar/SearchBarStyle.js
+++ src/components/SearchBar/SearchBarStyle.js
@@ -1,199 +1,266 @@
 import styled from 'styled-components'
 
-const StyledSearchBar = styled.nav`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-
-  position: fixed;
-  width: 100%;
-  height: 298px;
-  left: 0px;
-  top: 0px;
-  background: #570ff1;
-`
-
-export const StyledSearchContainer = styled.div`
+export const StyledSearchHeader = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  padding: 40px;
+  position: absolute;
+  width: 100%;
+  height: 18.625rem;
+  left: 0rem;
+  top: 0rem;
+  background: #570ff1;
+  margin: 0 auto;
 
-  margin-left: 13.6%;
-  margin-right: 16.25%;
-
-  position: static;
-  width: 1495px;
-  height: 213px;
-  left: 0px;
-  top: 85px;
-`
-
-export const StyledSearchHeaderContainer = styled.div`
-  position: static;
-  width: 1140px;
-  height: 62px;
-  left: 150px;
-  top: 40px;
-
-  font-family: 'PT Sans', sans-serif;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 48px;
-  line-height: 62px;
-
-  color: #ffffff;
-`
-
-export const StyledSearchBarContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0px 20px;
-
-  position: static;
-  width: 950px;
-  height: 60px;
-  left: 150px;
-  top: 113px;
-
-  background: #ffffff;
-  border: 1px solid #d2d0c9;
-  box-sizing: border-box;
-  box-shadow: 0px 4px 7px rgba(80, 120, 239, 0.2);
-  border-radius: 5px;
-`
-
-export const StyledSearchFormContainer = styled.div`
-  display: inline-block;
-  padding: 0px;
-
-  position: static;
-  width: 950px;
-  height: 83px;
-  left: 210px;
-  top: 18px;
-`
-
-export const StyledSearchFormInput = styled.input`
-  padding: 0px;
-  position: static;
-  width: 700px;
-  height: 24px;
-  left: 210px;
-  top: 18px;
-  border: 0;
-
-  input:focus,
-  textarea:focus,
-  select:focus {
-    outline: none;
+  h1 {
+    color: var(--white);
   }
 `
 
-export const StyledSearchButton = styled.button`
-  width: 24px;
-  height: 20px;
-  background: url('/search.png');
-  background-size: cover;
-  border: 0;
+export const StyledSearchContainer = styled.div`
+  max-width: var(--maxWidth);
+  position: relative;
+  justify-content: center;
+  text-align: left;
+  padding: 4rem 4.25rem;
+`
+/* Source: https://codepen.io/vkjgr/pen/VYMeXp */
+export const StyledDropdown = styled.div`
+  width: 40%;
+
+  select {
+    /* styling */
+    background-color: white;
+    border: solid 1px var(--white);
+    border-radius: 5px 0px 0px 5px;
+    color: var(--lightGray);
+    display: inline-block;
+    line-height: 1.5em;
+    padding: 0.5em 3.5em 0.5em 1em;
+    font-size: 1em;
+
+    /* reset */
+    margin: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
+  select.optionSelect {
+    background-image: linear-gradient(45deg, transparent 50%, gray 50%),
+      linear-gradient(135deg, gray 50%, transparent 50%),
+      linear-gradient(to right, #ccc, #ccc);
+    background-position: calc(100% - 25px) calc(1em + 2px),
+      calc(100% - 20px) calc(1em + 2px), calc(100% - 0.5em) 0.5em;
+    background-size: 5px 5px, 5px 5px, 1px 1.5em;
+    background-repeat: no-repeat;
+  }
+  select.optionSelect:focus {
+    outline: 0;
+  }
 `
 
-export const StyledSearchIcon = styled.div`
-  position: static;
-  width: 24px;
-  height: 24px;
-  left: 886px;
-  top: 0px;
-  float: right;
-`
+// export const StyledDropdownContainer = styled.form`
+//   display: flex;
+//   flex-flow: column;
+//   justify-content: flex-start;
+// `
 
-export const StyledSearchFiller = styled.div`
-  position: static;
-  height: 21px;
-  left: 0px;
-  top: 1.5px;
+// export const StyledSearchBar = styled.nav`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   padding: 0px;
 
-  font-family: PT Sans;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 21px;
-  /* identical to box height */
+//   position: fixed;
+//   width: 100%;
+//   height: 298px;
+//   left: 0px;
+//   top: 0px;
+//   background: #570ff1;
+// `
 
-  display: flex;
-  align-items: center;
+// export const StyledSearchContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 40px;
 
-  color: #aeb7d0;
-`
+//   margin-left: 13.6%;
+//   margin-right: 16.25%;
 
-export const StyledSearchDivider = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  padding: 5px 16px 5px 2px;
+//   position: static;
+//   width: 1495px;
+//   height: 213px;
+//   left: 0px;
+//   top: 85px;
+// `
 
-  position: static;
-  width: 18px;
-  height: 43px;
-  left: 192px;
-  top: 8.5px;
-`
+// export const StyledSearchHeaderContainer = styled.div`
+//   position: static;
+//   width: 1140px;
+//   height: 62px;
+//   left: 150px;
+//   top: 40px;
 
-export const StyledSearchDividerDiv = styled.div`
-  position: static;
-  width: 0px;
-  height: 42px;
-  left: 2px;
-  top: 5px;
-  border: 1px solid #aeb7d0;
-`
+//   font-family: 'PT Sans', sans-serif;
+//   font-style: normal;
+//   font-weight: bold;
+//   font-size: 48px;
+//   line-height: 62px;
 
-export const StyledSearchDropdown = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0px;
+//   color: #ffffff;
+// `
 
-  position: static;
-  width: 172px;
-  height: 22px;
-  left: 20px;
-  top: 19px;
-`
+// export const StyledSearchBarContainer = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   align-items: center;
+//   padding: 0px 20px;
 
-export const StyledSearchDropdownArrow = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  padding: 0px;
+//   position: static;
+//   width: 950px;
+//   height: 60px;
+//   left: 150px;
+//   top: 113px;
 
-  position: static;
-  width: 22px;
-  height: 22px;
-  left: 150px;
-  top: 0px;
-`
+//   background: #ffffff;
+//   border: 1px solid #d2d0c9;
+//   box-sizing: border-box;
+//   box-shadow: 0px 4px 7px rgba(80, 120, 239, 0.2);
+//   border-radius: 5px;
+// `
 
-export const StyledSearchDropdownText = styled.div`
-  position: static;
-  width: 23px;
-  height: 18px;
-  left: 0px;
-  top: 2px;
+// export const StyledSearchFormContainer = styled.div`
+//   display: inline-block;
+//   padding: 0px;
 
-  font-family: PT Sans;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 14px;
-  line-height: 18px;
-  display: flex;
-  align-items: center;
+//   position: static;
+//   width: 950px;
+//   height: 83px;
+//   left: 210px;
+//   top: 18px;
+// `
 
-  color: #aeb7d0;
-`
+// export const StyledSearchFormInput = styled.input`
+//   padding: 0px;
+//   position: static;
+//   width: 700px;
+//   height: 24px;
+//   left: 210px;
+//   top: 18px;
+//   border: 0;
 
-export default StyledSearchBar
+//   input:focus,
+//   textarea:focus,
+//   select:focus {
+//     outline: none;
+//   }
+// `
+
+// export const StyledSearchButton = styled.button`
+//   width: 24px;
+//   height: 20px;
+//   background: url('/search.png');
+//   background-size: cover;
+//   border: 0;
+// `
+
+// export const StyledSearchIcon = styled.div`
+//   position: static;
+//   width: 24px;
+//   height: 24px;
+//   left: 886px;
+//   top: 0px;
+//   float: right;
+// `
+
+// export const StyledSearchFiller = styled.div`
+//   position: static;
+//   height: 21px;
+//   left: 0px;
+//   top: 1.5px;
+
+//   font-family: PT Sans;
+//   font-style: normal;
+//   font-weight: normal;
+//   font-size: 16px;
+//   line-height: 21px;
+//   /* identical to box height */
+
+//   display: flex;
+//   align-items: center;
+
+//   color: #aeb7d0;
+// `
+
+// export const StyledSearchDivider = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   align-items: flex-start;
+//   padding: 5px 16px 5px 2px;
+
+//   position: static;
+//   width: 18px;
+//   height: 43px;
+//   left: 192px;
+//   top: 8.5px;
+// `
+
+// export const StyledSearchDividerDiv = styled.div`
+//   position: static;
+//   width: 0px;
+//   height: 42px;
+//   left: 2px;
+//   top: 5px;
+//   border: 1px solid #aeb7d0;
+// `
+
+// export const StyledSearchDropdown = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+//   align-items: center;
+//   padding: 0px;
+
+//   position: static;
+//   width: 172px;
+//   height: 22px;
+//   left: 20px;
+//   top: 19px;
+// `
+
+// export const StyledSearchDropdownArrow = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   align-items: flex-start;
+//   padding: 0px;
+
+//   position: static;
+//   width: 22px;
+//   height: 22px;
+//   left: 150px;
+//   top: 0px;
+// `
+
+// export const StyledSearchDropdownText = styled.div`
+//   position: static;
+//   width: 23px;
+//   height: 18px;
+//   left: 0px;
+//   top: 2px;
+
+//   font-family: PT Sans;
+//   font-style: normal;
+//   font-weight: normal;
+//   font-size: 14px;
+//   line-height: 18px;
+//   display: flex;
+//   align-items: center;
+
+//   color: #aeb7d0;
+// `
+
+export default StyledSearchHeader
