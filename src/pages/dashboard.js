6c0d702a9828b2413301fa07e157b93cf7e4f3f0
Rebekah <koonr@oregonstate.edu>
2021-05-08 13:31:48 -0700

    text for side bar

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -17,8 +17,11 @@ const DashboardView = () => {
         <SearchBar headerText={headerText} />
         <StyledDashboardBody>
           <StyledSideBar>
-            UwU UwU UwU UwU UwU UwU UwU UwU UwU UwU UwU UwU UwU UwU UwU UwU UwU
-            UwU UwU UwU UwU
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id
+            justo felis. Proin tristique ligula ut odio faucibus tincidunt. Sed
+            et lectus sed tortor ultricies hendrerit. In hac habitasse platea
+            dictumst. Pellentesque eget suscipit mi. Cras aliquam nulla vitae
+            blandit dapibus. Sed ornare elit viverra nisl aliquet pretium.
           </StyledSideBar>
           <StyledResume>
             <Education />
