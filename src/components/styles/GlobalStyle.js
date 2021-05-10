c40c40e6d847414d38c06c9dbd50a15cf54c462b
Rebekah <koonr@oregonstate.edu>
2021-05-09 20:06:31 -0700

    trying to fix icon sizes

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -1,4 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
 
 const GlobalStyle = createGlobalStyle`
   :root {
