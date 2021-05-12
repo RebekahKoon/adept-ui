b63818f202593a3a6ad860084b8ded1900f21614
devnguy <hdn301@gmail.com>
2021-05-12 18:50:31 -0500

    env file, setup ApolloClient to get token from header, setup next-auth

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,31 @@
-import { ApolloClient, ApolloProvider } from '@apollo/client'
+import { ApolloClient, createHttpLink, ApolloProvider } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { cache } from '../../apollo/cache'
 import Footer from '../Footer'
 import NavBar from '../NavBar'
 import { Meta } from '../Meta'
 import { GlobalStyle } from '../../components/styles'
 
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER,
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token')
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
 // Initialize ApolloClient
-// uri needs to be changed to accommodate production
 const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache,
-  uri: 'http://localhost:4000/graphql',
 })
 
 const Layout = ({ children, hasNav = true, hasFooter = true }) => {
