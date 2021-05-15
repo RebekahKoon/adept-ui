34ce82ed798e649e02ee165bceea89bc5c13a57e
Nathan <Nathan.shelby30@gmail.com>
2021-05-15 12:08:03 -0700

    laying groundwork for individual job page

--- src/pages/job-posting.js
+++ src/pages/job-posting.js
@@ -1,9 +1,106 @@
 // pages/job-posting.js
+import Router from 'next/router'
+
 import Layout from '../components/Layout'
+import SearchBar from '../components/SearchBar'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import MainContentFlexContainer from '../components/styles/MainContentFlexContainer'
+import JobPostingNavContainer from '../styles/JobPostingStyle'
+import {
+  JobPostingNavContent,
+  JobPostingReturn,
+  JobPostingReturnText,
+  JobPostingReturnArrow,
+  JobPostingBody,
+  JobPostingBodyMain,
+  JobPostingBodyMainContentContainer,
+  JobPostingBodyHeader,
+  JobPostingBodyHeaderLogo,
+  JobPostingBodyHeaderInfoContainer,
+  JobPostingBodyHeaderDate,
+  JobPostingBodyHeaderInfo,
+  JobPostingInfoText,
+  JobPostingBodyHeaderPosition,
+  JobPostingBodyHeaderCompany,
+  JobPostingBodyResult,
+  JobPostingBodyResultDescription,
+} from '../styles/JobPostingStyle'
+
+const handleClick = (e) => {
+  e.preventDefault()
+  Router.push('/search-results')
+}
 const JobPostingView = (props) => (
   <Layout>
-    <MainContentFlexContainer>JobPosting</MainContentFlexContainer>
+    <SearchBar />
+    <MainContentFlexContainer>
+      <JobPostingNavContainer>
+        <JobPostingNavContent>
+          <JobPostingReturn onClick={handleClick}>
+            <JobPostingReturnArrow>
+              <i className="fas fa-arrow-left"></i>
+            </JobPostingReturnArrow>
+            <JobPostingReturnText>Search Results</JobPostingReturnText>
+          </JobPostingReturn>
+        </JobPostingNavContent>
+      </JobPostingNavContainer>
+      <JobPostingBody>
+        <JobPostingBodyMain>
+          <JobPostingBodyMainContentContainer>
+            <JobPostingBodyHeader>
+              <JobPostingBodyHeaderLogo>
+                <i className="fab fa-adn"></i>
+              </JobPostingBodyHeaderLogo>
+              <JobPostingBodyHeaderInfoContainer>
+                <JobPostingBodyHeaderCompany>
+                  Company Name
+                </JobPostingBodyHeaderCompany>
+                <JobPostingBodyHeaderPosition>
+                  A Very Long Remote Position Job Title Taking Up Space
+                </JobPostingBodyHeaderPosition>
+                <JobPostingBodyHeaderInfo>
+                  <JobPostingInfoText>
+                    <i className="fas fa-suitcase"></i>
+                    <p>Text</p>
+                  </JobPostingInfoText>
+                  <JobPostingInfoText>
+                    <i className="fas fa-map-marker-alt"></i>
+                    <p>Text</p>
+                  </JobPostingInfoText>
+
+                  <JobPostingInfoText>
+                    <i className="fas fa-clock"></i>
+                    <p>Text</p>
+                  </JobPostingInfoText>
+
+                  <JobPostingInfoText>
+                    <i className="fas fa-dollar-sign"></i>
+                    <p>Text</p>
+                  </JobPostingInfoText>
+                </JobPostingBodyHeaderInfo>
+                <JobPostingBodyHeaderDate>
+                  Posted on April18th, 2021
+                </JobPostingBodyHeaderDate>
+              </JobPostingBodyHeaderInfoContainer>
+            </JobPostingBodyHeader>
+            <JobPostingBodyResult>
+              <JobPostingBodyResultDescription>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum
+              </JobPostingBodyResultDescription>
+            </JobPostingBodyResult>
+          </JobPostingBodyMainContentContainer>
+        </JobPostingBodyMain>
+      </JobPostingBody>
+    </MainContentFlexContainer>
   </Layout>
 )
 
--- src/styles/JobPostingStyle.js
+++ src/styles/JobPostingStyle.js
@@ -1,1 +1,243 @@
 import styled from 'styled-components'
+
+const JobPostingNavContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: relative;
+  width: 100%;
+  height: 64px;
+  left: 0px;
+  top: 0px;
+`
+
+export const JobPostingNavContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+
+  position: static;
+  width: 100%;
+  height: 64px;
+  left: 0px;
+  top: 0px;
+`
+
+export const JobPostingReturn = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 138px;
+  height: 24px;
+  left: 20px;
+  top: 20px;
+`
+
+export const JobPostingReturnText = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px 0px 0px 8px;
+
+  position: static;
+  width: 114px;
+  height: 24px;
+  left: 24px;
+  top: 0px;
+`
+
+export const JobPostingReturnArrow = styled.div`
+  position: static;
+  width: 24px;
+  height: 24px;
+  left: 0px;
+  top: 0px;
+`
+
+export const JobPostingBody = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 40px 0px 128px;
+
+  position: relative;
+  width: 100%;
+  height: 1061px;
+  left: 0px;
+  top: 64px;
+`
+
+export const JobPostingBodyMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 1140px;
+  height: 893px;
+  left: 150px;
+  top: 40px;
+`
+export const JobPostingBodyMainContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 16.5px 40px;
+
+  position: static;
+  width: 1140px;
+  height: 893px;
+  left: 0px;
+  top: 0px;
+`
+
+export const JobPostingBodyHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 1060px;
+  height: 309px;
+  left: 40px;
+  top: 16.5px;
+`
+
+export const JobPostingBodyHeaderLogo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px 0px 40px;
+
+  position: static;
+  width: 64px;
+  height: 104px;
+  left: 0px;
+  top: 0px;
+`
+
+export const JobPostingBodyHeaderInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 100%;
+  height: 205px;
+  left: 0px;
+  top: 104px;
+`
+
+export const JobPostingBodyHeaderDate = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 40px 0px 12px;
+
+  position: relative;
+  width: 169px;
+  height: 73px;
+  left: 0px;
+  top: 132px;
+`
+
+export const JobPostingBodyHeaderInfo = styled.div`
+  flex-direction: row;
+  align-items: flex-start;
+
+  position: static;
+  width: 100%;
+  height: 60px;
+  left: 0px;
+  top: 108px;
+`
+
+export const JobPostingInfoText = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-right: 1rem;
+  width: 5%;
+  height: 100%;
+`
+
+export const JobPostingBodyHeaderPosition = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px 0px 8px;
+
+  position: static;
+  width: 992px;
+  height: 68px;
+  left: 0px;
+  top: 40px;
+`
+
+export const JobPostingBodyHeaderCompany = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px 0px 4px;
+
+  position: static;
+  width: 167px;
+  height: 40px;
+  left: 0px;
+  top: 0px;
+`
+
+export const JobPostingBodyHeaderJobInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 1060px;
+  height: 205px;
+  left: 0px;
+  top: 104px;
+`
+
+export const JobPostingBodyResult = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: relative;
+  width: 600px;
+  height: 551px;
+  left: 40px;
+  top: 325.5px;
+
+  border-radius: 5px;
+`
+
+export const JobPostingBodyResultDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: relative;
+  width: 600px;
+  height: 180px;
+  left: 0px;
+  top: 0px;
+`
+
+export default JobPostingNavContainer
