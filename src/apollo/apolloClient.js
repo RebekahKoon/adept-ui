a0e803d693a024c5dc7973cee2da348affcbc250
Rebekah <koonr@oregonstate.edu>
2021-05-15 14:21:16 -0700

    apollo client in separate file

---
+++ src/apollo/apolloClient.js
@@ -0,0 +1,27 @@
+import { ApolloClient, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { cache } from './cache'
+
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER,
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token')
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+// Initialize ApolloClient
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+})
+
+export default client
