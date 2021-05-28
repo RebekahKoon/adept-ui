ee0fe89a6bc0c33fbcb68e764699ff1e20999add
Nathan <Nathan.shelby30@gmail.com>
2021-05-28 16:25:09 -0700

    Starting work on User search

---
+++ src/components/SkillDropdown/SearchSkillDropdown.js
@@ -0,0 +1,36 @@
+import CreatableSelect from 'react-select/creatable'
+import StyledSkillDropdown from './SkillDropdownStyle'
+
+const SearchSkillDropdown = ({
+  placeholderText,
+  isLoading,
+  setIsLoading,
+  skills,
+  setSkills,
+  newSkill,
+  setNewSkill,
+}) => {
+  // Used to determine if the dropdown value has changed
+  const handleChange = (newValue, actionMeta) => {
+    console.group('Value Changed')
+    console.log(newValue)
+    console.log(`action: ${actionMeta.action}`)
+    console.groupEnd()
+    setNewSkill(newValue)
+  }
+
+  return (
+    <CreatableSelect
+      placeholder={placeholderText}
+      isClearable
+      isDisabled={isLoading}
+      isLoading={isLoading}
+      onChange={handleChange}
+      options={skills}
+      value={newSkill}
+      styles={StyledSkillDropdown}
+    />
+  )
+}
+
+export default SearchSkillDropdown
--- src/components/SkillDropdown/index.js
+++ src/components/SkillDropdown/index.js
@@ -1,6 +1,7 @@
 import SkillDropdown from './SkillDropdown'
 import RequiredSkillsDropdown from './RequiredSkillsDropdown'
 import StyledSkillDropdown from './SkillDropdownStyle'
+import SearchSkillDropdown from './SearchSkillDropdown'
 
 export default SkillDropdown
-export { RequiredSkillsDropdown, StyledSkillDropdown }
+export { RequiredSkillsDropdown, SearchSkillDropdown, StyledSkillDropdown }
