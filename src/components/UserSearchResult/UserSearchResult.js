069cc0d6224a1c39e7e0928237f98ee4e8868d1d
Nathan <Nathan.shelby30@gmail.com>
2021-05-29 09:38:58 -0700

    Add Contact button adds contact and loads

--- src/components/UserSearchResult/UserSearchResult.js
+++ src/components/UserSearchResult/UserSearchResult.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Router from 'next/router'
+import Loader from 'react-loader-spinner'
+
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
-
+import { useMutation } from '@apollo/client'
+import { ADD_CONTACT_TO_USER } from '../../queries/addContactToUser'
+import useUser from '../../lib/useUser'
 import SSRSearchResultDiv from './UserSearchResultStyle'
 import {
   SSRSearchResultFooter,
@@ -27,20 +31,27 @@ import {
 } from './UserSearchResultStyle'
 
 function UserSearchResult(props) {
+  const { user } = useUser()
   console.log(props.data)
   const dataArr = props.data.skills
   console.log(dataArr)
-  const handleClick = (e) => {
-    e.preventDefault()
-    var currPage = props.currPage - 1
-    var id = currPage * 12 + props.id
-    const jid = '0d8f2554-d0a3-4f1a-a5ba-caed21b9eb1a'
-    if (props.q) {
-      // Router.push('/job-posting?q=' + props.q + '&id=' + id)
-      Router.push(`/job-posting?id=${jid}`)
-    } else {
-      Router.push(`/job-posting?id=${jid}`)
-    }
+
+  const [
+    addContactToUser,
+    { loading: addSkillToUserLoading, error: addSkillToUserError },
+  ] = useMutation(ADD_CONTACT_TO_USER, {
+    onCompleted({ addContactToUser }) {
+      console.log('User added')
+    },
+    onError(e) {
+      console.log(e)
+    },
+  })
+
+  const handleClick = () => {
+    addContactToUser({
+      variables: { userId: user.userId, contactId: props.data.userId },
+    })
   }
 
   const createSkillDivs = () =>
@@ -109,9 +120,7 @@ function UserSearchResult(props) {
               <i className="fab fa-adn"></i>
             </SSRJobLogoContainer>
             <SSRMainContent>
-              <SSRJobTitleContainer>
-                {props.data.positionTitle}
-              </SSRJobTitleContainer>
+              <SSRJobTitleContainer>{props.data.name}</SSRJobTitleContainer>
               <SSRJobInfoContainer>
                 <SSRCompanyContainer>
                   <SSRCompanyTextContainer>
@@ -140,10 +149,27 @@ function UserSearchResult(props) {
           </SSRJobInfoAndLogo>
         </SSRSearchResultContent>
         <SSRSearchResultLinkContainer>
-          <SSRJobButton value="View Job" label="ViewJob" onClick={handleClick}>
-            View Job
-            <i className="fas fa-arrow-right"></i>
-          </SSRJobButton>
+          {addSkillToUserLoading ? (
+            <div
+              style={{
+                display: 'flex',
+                width: '25%',
+                marginLeft: '5px',
+                justifyContent: 'center',
+              }}
+            >
+              <Loader type="TailSpin" color="#570EF1" height={32} width={32} />
+            </div>
+          ) : (
+            <SSRJobButton
+              value="View Job"
+              label="ViewJob"
+              onClick={handleClick}
+            >
+              Add Contact
+              <i className="fas fa-plus"></i>
+            </SSRJobButton>
+          )}
         </SSRSearchResultLinkContainer>
       </SSRSearchResultContainer>
       <SSRSearchResultFooter>
